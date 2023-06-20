7bbc07dda2f911ed93e9a008ee8333bd44f2064e
Ethan Jones <ethan.nel.jones@gmail.com>
2023-06-19 21:51:24 -0400

    integrated copyfiles to copy other static assets --> html, css, images, etc...

--- index.ts
+++ index.ts
@@ -1,29 +1,17 @@
-// // express
-// import express, { Express } from 'express';
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// const app: Express = express();
-// const PORT = 3000;
-
-// // middleware
-// app.use(express.static(__dirname + "dist"));
-// app.get('/', (req, res) => {
-//    console.log(res)
-// })
-// app.listen(PORT, () => console.log(`listening on --> http://localhost:${PORT}`, ));
-
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const app: Express = express();
+const app = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-//   res.send('Express + TypeScript Server is running with nodemon');
-        res.sendFile(__dirname + "/public/index.html")
-});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+app.get('/', (_, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
+// app.use('public', express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => console.log(`server started at localhost:${port}`));
